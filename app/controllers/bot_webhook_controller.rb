b12b16a2742e9104749836802a2f33348f726628
Remi Duvoux <remi.duvoux@gmail.com>
2017-12-05 17:26:09 +0100

    webhook controller 1

--- app/controllers/bot_webhook_controller.rb
+++ app/controllers/bot_webhook_controller.rb
@@ -2,15 +2,15 @@ class BotWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def answer
-    # @params = JSON.parse(request.body.read)
-    content_type :json
-    {
-      replies: [{ type: 'text', content: 'Roger that' }],
-      conversation: {
-        memory: {
-          key: 'value'
-        }
-      }
-    }.to_json
+    @params = JSON.parse(request.body.read)
+    # content_type :json
+    # {
+    #   replies: [{ type: 'text', content: 'Roger that' }],
+    #   conversation: {
+    #     memory: {
+    #       key: 'value'
+    #     }
+    #   }
+    # }.to_json
   end
 end
