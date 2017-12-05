a8a5503e79dcc2379db99b37e29a98bed2f681d3
Remi Duvoux <remi.duvoux@gmail.com>
2017-12-05 18:32:47 +0100

    trying with .json.erb template

--- app/controllers/bot_webhook_controller.rb
+++ app/controllers/bot_webhook_controller.rb
@@ -2,15 +2,7 @@ class BotWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def answer
-    @params = JSON.parse(request.body.read)
-    # content_type :json
-    # {
-    #   replies: [{ type: 'text', content: 'Roger that' }],
-    #   conversation: {
-    #     memory: {
-    #       key: 'value'
-    #     }
-    #   }
-    # }.to_json
+    # @params = JSON.parse(params.to_s)
+    content_type :json
   end
 end
