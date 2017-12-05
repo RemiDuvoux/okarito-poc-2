0976e7f6fc59e1ee400a96bc2c27e89fdc8d5eab
Remi Duvoux <remi.duvoux@gmail.com>
2017-12-05 19:37:24 +0100

    add recast gem for smart speech management

--- app/controllers/bot_webhook_controller.rb
+++ app/controllers/bot_webhook_controller.rb
@@ -4,15 +4,22 @@ class BotWebhookController < ApplicationController
   def answer
     require 'json'
 
-    @answer = {
-      "replies": [{ "type": "text", "content": "Roger that" }],
-      "conversation": {
-        "memory": {
-          "key": "value"
-        }
-      }
-    }.to_json
-    render json: @answer
+    require 'recastai'
+
+    client = RecastAI::Client.new(ENV['recast_token'])
+
+    client.request.analyse_text('Hi')
+    client.connect.broadcast_message('Hello')
+
+    # @answer = {
+    #   "replies": [{ "type": "text", "content": "Roger that" }],
+    #   "conversation": {
+    #     "memory": {
+    #       "key": "value"
+    #     }
+    #   }
+    # }.to_json
+    # render json: @answer
 
     # @params = JSON.parse(params.to_s)
   end
