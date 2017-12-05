37346a8ce4258e997ebbc084c7216fce643cec51
Remi Duvoux <remi.duvoux@gmail.com>
2017-12-05 17:22:30 +0100

    change db connection + some changes in webhook controller

--- app/controllers/bot_webhook_controller.rb
+++ app/controllers/bot_webhook_controller.rb
@@ -2,7 +2,7 @@ class BotWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def answer
-    @params = JSON.parse(request.body.read)
+    # @params = JSON.parse(request.body.read)
     content_type :json
     {
       replies: [{ type: 'text', content: 'Roger that' }],
